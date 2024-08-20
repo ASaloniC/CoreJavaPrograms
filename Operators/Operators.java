728529d57ef9e92a95a5752e35bfd3edaf0adbe5
Saloni Jain <salonijain0601@gmail.com>
2024-08-20 13:50:43 +0530

    operator file

---
+++ Operators/Operators.java
@@ -0,0 +1,95 @@
+public class Operators {
+
+    public static void main(String args[]){  
+
+        /* Unary Operator */
+        int x=10;  
+        boolean c = true;
+
+        //PostFix
+        //System.out.println(x++); 
+        //System.out.println(x--); 
+        //System.out.println(x);  
+
+        //PreFix
+        //System.out.println(++x);
+        //System.out.println(--x);  
+
+
+        //System.out.println(x++ + ++x);   //10+12=22  
+        //System.out.println(x++ + x++);   //10+11=21  
+
+        //System.out.println(~x);
+        //System.out.println(!c);
+
+
+        /*  Arithmetic Operator */
+        int a=10;  
+        int b=5;  
+  
+        /*
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        */
+
+        /*  Shift Operator */
+
+        //Left Shift
+        //System.out.println(10<<2);   //10*2^2=10*4=40  
+        //System.out.println(10<<3);   //10*2^3=10*8=80  
+
+        //Right Shift
+        //System.out.println(10>>2);   //10/2^2=10/4=2  
+        //System.out.println(20>>2);   //20/2^2=20/4=5  
+
+        /*  AND Operator */
+        int d=10;  
+        int e=5;  
+        int f=20;  
+
+        /*
+        System.out.println(d<e && d<f);
+        System.out.println(d<e & d<f);  
+        */
+
+        /* OR Operator */
+
+        /*
+        System.out.println(d>e || d<f);
+        System.out.println(d>e | d<f);
+        */
+
+        /* Ternary Operator */
+        int g=2;  
+        int h=5;  
+
+        /* 
+        int min=(g<h) ? g : h;  
+        System.out.println(min); 
+        */
+
+        
+        /* Assignment Operator */
+        int i=10;
+        
+        /* 
+        i+= 3;
+        System.out.println(i);
+
+        i-= 4; 
+        System.out.println(i);  
+
+        i*= 2; 
+        System.out.println(i); 
+
+        i/= 2;
+        System.out.println(i);  
+
+        */
+
+
+    }
+}
