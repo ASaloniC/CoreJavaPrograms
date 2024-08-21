3c1a812de782057b463c6ce256d6f68ddd2a24ec
Saloni Jain <salonijain0601@gmail.com>
2024-08-21 12:38:32 +0530

    Control Statements

---
+++ ControlStatements/DecisionMaking.java
@@ -0,0 +1,67 @@
+public class DecisionMaking
+   
+  {
+  public static void main(String args[])
+    {
+
+        /* If else Statement */
+        /* 
+        int x = 10;  
+        int y = 12;  
+
+        if(x+y < 10) 
+        {  
+          System.out.println("x + y is less than 10");  
+        }   
+        else 
+        {  
+          System.out.println("x + y is greater than 10");  
+        }  
+        */
+        
+        
+        /*  if-else-if ladder  */
+        /* 
+        String city = "Delhi";  
+        
+        if(city == "Meerut") 
+        { 
+          System.out.println("city is Meerut");  
+        }
+        else if (city == "Noida") 
+        {  
+          System.out.println("city is noida");  
+        }
+        else if(city == "Agra") 
+        { 
+          System.out.println("city is agra");  
+        }
+        else 
+        {  
+          System.out.println(city); 
+        } 
+        */
+
+        /*  Nested if-statement  */
+        /* 
+          int num=1;
+
+	        if(num<10)
+	        {
+		        if(num==1)
+		        {
+			        System.out.println("The value is : "+num);
+		        }
+		        else
+		        {
+			        System.out.println("The value is greater than 1");
+		        }
+	        }
+	        else
+	        {
+            System.out.println("The value is greater than 10");
+	        }
+          */
+        
+    }
+ }
